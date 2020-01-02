70a5d9ed4b69cfc391a362a363d3785d691079f5
YeFFreY <johan.mslms@outlook.com>
2020-01-02 20:09:22 +0100

    Test the GET and POST of ApiService

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ApiService } from './api.service';
 import { Resource } from '../../lib/hateoas';
+import { HttpClient } from '@angular/common/http';
 
 describe('ApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [ HttpClientTestingModule ]
-  }));
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
+    // Inject the http service and test controller for each test
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
 
   it('should be created', () => {
     const service: ApiService = TestBed.get(ApiService);
@@ -31,5 +45,39 @@ describe('ApiService', () => {
         rel2: '/api/rel2',
       }
     );
+  });
+
+  it('should be able to retrieve resource with a valid REL', (done: DoneFn) => {
+    const apiIndex: Resource = { id: 'api', _links: [ { rel: 'rel1', href: '/api/rel1' } ] };
+    const testData: Resource = { id: 1, _links: [] };
+    const service: ApiService = TestBed.get(ApiService);
+    service.get('rel1').subscribe(value => {
+      expect(value).toBe(testData);
+      done();
+    });
+
+    const apiReq = httpTestingController.expectOne('api/index');
+    expect(apiReq.request.method).toEqual('GET');
+    apiReq.flush(apiIndex);
+    const resourceReq = httpTestingController.expectOne('/api/rel1');
+    expect(resourceReq.request.method).toEqual('GET');
+    resourceReq.flush(testData);
+  });
+
+  it('should be able to post data with a valid REL', (done: DoneFn) => {
+    const apiIndex: Resource = { id: 'api', _links: [ { rel: 'rel1', href: '/api/rel1' } ] };
+    const testData: Resource = { id: 'posted', _links: [] };
+    const service: ApiService = TestBed.get(ApiService);
+    service.post('rel1', {}).subscribe(value => {
+      expect(value).toBe(testData);
+      done();
+    });
+
+    const apiReq = httpTestingController.expectOne('api/index');
+    expect(apiReq.request.method).toEqual('GET');
+    apiReq.flush(apiIndex);
+    const resourceReq = httpTestingController.expectOne('/api/rel1');
+    expect(resourceReq.request.method).toEqual('POST');
+    resourceReq.flush(testData);
   });
 });
